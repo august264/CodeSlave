e04196ddc0b2c7d5d382b3ff0fd5c4534f657e86
august264 <jli337@student.monash.edu>
2018-11-06 12:23:57 +1100

    Initial commit

---
+++ Source/CodeSlave/CodeSlave.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "CodeSlave.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CodeSlave, "CodeSlave" );
